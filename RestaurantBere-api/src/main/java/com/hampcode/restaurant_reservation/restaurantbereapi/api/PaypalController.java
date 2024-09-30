b26e3d10c38734990df96931dfe399f0fcdc33a3
javierUPAO <jaguilarb3@upao.edu.pe>
2024-09-30 02:40:20 -0500

    Enrutando front virtual con back virtual

--- RestaurantBere-api/src/main/java/com/hampcode/restaurant_reservation/restaurantbereapi/api/PaypalController.java
+++ RestaurantBere-api/src/main/java/com/hampcode/restaurant_reservation/restaurantbereapi/api/PaypalController.java
@@ -90,7 +90,7 @@ public class PaypalController {
         }
 
         // Asegúrate de que el token se almacene después de la creación de la orden
-        String returnUrl = "http://localhost:8080/api/v1/reservasion/dia/mesas/menu/datos/pay-reservation/success";
+        String returnUrl = "https://restaurantbere-52059.web.app/api/v1/reservasion/dia/mesas/menu/datos/pay-reservation/success";
         String cancelUrl = "https://blog.fluidui.com/top-404-error-page-examples/";
         double totalpagar = reservation.getPriceTotal();
 
@@ -142,7 +142,7 @@ public class PaypalController {
             reservationConfirmationImpl.sendReservationEmail(bccRecipients, reservationResponseDTO); // envía el correo
 
             // Redirigir a una URL del frontend
-            String redirectUrl = "http://localhost:4200/reservasion/mesas/menu/datos/resumen/pago-completado"; // Cambia esto a la URL de tu frontend
+            String redirectUrl = "https://restaurantbere-52059.web.app/reservasion/mesas/menu/datos/resumen/pago-completado"; // Cambia esto a la URL de tu frontend
             response.sendRedirect(redirectUrl); // Redirige al cliente
         } else {
             response.getWriter().write("Pago completado con éxito.");
